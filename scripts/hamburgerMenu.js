b022eac9b0dc44f7d19df81eeef9bf4ebb166e62
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-01-29 21:22:50 -0600

    dark mode

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -5,3 +5,12 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
