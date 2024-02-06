26f378afd1d7471646179d87faf4abb3ca397e11
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-02-06 17:55:28 -0600

    fixed errors

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -1,47 +1,52 @@
-//// new stuff
-
-//declared a new array
-const chaptersArray = getChapterList() || [];
-
-
-
-//selec elements using their ids
+// Select elements using their ids
 const inputElement = document.querySelector("#favchap");
 const buttonElement = document.querySelector("#addChapter");
 const listElement = document.querySelector("#list");
 
+let chaptersArray = getChapterList() || [];
+
 // part 4
 chaptersArray.forEach(chapter => {
     displayList(chapter);
-  });
-
-//click event listener
-buttonElement.addEventListener("click", () => {
-
-    //checking if the input is not blank
-    if (inputElement.value !== "") {
-        //create a new list item 
-        const li = document.createElement("li");
-        //create delete button for the list
-        const deleteButton = document.createElement("button");
-
-        //setting text content for the delete button and list item to input the value
-        li.textContent = inputElement.value;
-        deleteButton.textContent = "❌";
-
-        //adding/appending the deletebutton
-        li.append(deleteButton);
-        //'' the list item to the list
-        listElement.append(li);
-
-        //adding a click event to the x button
-        deleteButton.addEventListener("click", () => {
-            listElement.removeChild(li);
-            inputElement.focus();
-        });
-    }
-    //clearing input value
-    inputElement.value = "";
-    //retutning to the input element
-    inputElement.focus();
 });
+
+buttonElement.addEventListener('click', () => {
+    if (inputElement.value !== '') {      //checking if the input is not blank
+        displayList(inputElement.value); //call the function that outputs the submitted chapter
+        chaptersArray.push(inputElement.value);  //add the chapter to the array
+        setChapterList(); //update the local storage with the new array
+        inputElement.value = ''; //clearing input value
+        inputElement.focus();//returning to the input element
+    }
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item;
+
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete');
+    li.append(deletebutton);
+    listElement.append(li);
+    deletebutton.addEventListener('click', function () {
+        listElement.removeChild(li);
+        deleteChapter(li.textContent);
+        inputElement.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('favChap', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    const storedData = localStorage.getItem('favChap');
+    return storedData ? JSON.parse(storedData) : null;
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
