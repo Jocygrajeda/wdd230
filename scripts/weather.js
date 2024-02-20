e1d267655641118d622e0f6e3683b69d65650ad6
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-02-19 19:42:32 -0600

    weather

---
+++ scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Nacogdoches,us&units=imperial&appid=a8ce9bd9b01902318a715c79a9bdf2a6';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
