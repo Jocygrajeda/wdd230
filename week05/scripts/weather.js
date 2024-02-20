c1dd8e262e5319f5dfabac85b8ad7df83d20e195
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-02-19 18:56:07 -0600

    missing letter fixed

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,8 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64units=imperial&appid=a8ce9bd9b01902318a715c79a9bdf2a6'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=a8ce9bd9b01902318a715c79a9bdf2a6';
+
 
 async function apiFetch() {
     try{
