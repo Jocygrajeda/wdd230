bcf3442d219678232f4e82d96107a67b53a97aa9
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-02-20 13:46:35 -0600

    idk

--- scripts/links.js
+++ scripts/links.js
@@ -1,32 +1,20 @@
-const baseURL = "https://jocygrajeda.github.io/wdd230";
+const baseURL = "https://jocygrajeda.github.io/wdd230/";
 const linksURL = "https://jocygrajeda.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data.weeks);
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data);
 }
 
 function displayLinks(weeks) {
-    const list = document.querySelector(".links");
-  
-    weeks.forEach((week) => {
-      const listElement = document.createElement("li");
-      listElement.innerHTML = `<strong>${week.week}</strong>`;
-  
-      const linkList = document.createElement("ul");
-  
-      week.links.forEach((link) => {
-        const listItem = document.createElement("li");
-        const linkElement = document.createElement("a");
-        linkElement.setAttribute("href", baseURL + link.url);
-        linkElement.textContent = link.title;
-  
-        listItem.appendChild(linkElement);
-        linkList.appendChild(listItem);
-      });
-  
-      listElement.appendChild(linkList);
-      list.appendChild(listElement);
-    });
-  }
+  weeks.forEach((week) => {
+    const weekLink = document.createElement('a');
+    weekLink.href = week.links.url;
+    weekLink.textContent = week.links.title;
+
+    document.body.appendChild(weekLink);
+  });
+}
+
+getLinks();
