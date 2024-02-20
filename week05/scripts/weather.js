2cb12e6ccbf30d400f4181f6780e38a0c9fac377
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-02-19 18:34:17 -0600

    weather

---
+++ week05/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64units=imperial&appid=a8ce9bd9b01902318a715c79a9bdf2a6'
