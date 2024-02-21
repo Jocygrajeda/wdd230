5cb4c019010262e514fc8d8e1b9c177b29b296d1
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-02-21 14:32:19 -0600

    weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -22,15 +22,9 @@ apiFetch();
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    
-    let desc = data.weather[0].description;
-    
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed;
-    
-    captionDesc.textContent = `${desc}. Humidity: ${humidity}%. Wind Speed: ${windSpeed} mph.`;
-    
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
 }
